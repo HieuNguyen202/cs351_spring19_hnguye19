ed34b3aea366cf653bb252cd2abc72f9ede27c3b
Hieu <edwardnguyen202@gmail.com>
2019-01-25 11:41:47 -0600

    Add function description/requirements

--- mps/02/hashtable.c
+++ mps/02/hashtable.c
@@ -14,6 +14,9 @@ unsigned long hash(char *str) {
   return hash;
 }
 
+/// Creates a hashtable with size buckets.
+/// \param size
+/// \return
 hashtable_t *make_hashtable(unsigned long size) {
   hashtable_t *ht = malloc(sizeof(hashtable_t));
   ht->size = size;
@@ -21,6 +24,10 @@ hashtable_t *make_hashtable(unsigned long size) {
   return ht;
 }
 
+/// Inserts the key => val mapping, or updates the value for key, if it is already in the hashtable.
+/// \param ht
+/// \param key
+/// \param val
 void ht_put(hashtable_t *ht, char *key, void *val) {
   /* FIXME: the current implementation doesn't update existing entries */
   unsigned int idx = hash(key) % ht->size;
@@ -31,6 +38,10 @@ void ht_put(hashtable_t *ht, char *key, void *val) {
   ht->buckets[idx] = b;
 }
 
+/// Returns the value for key, or NULL if key doesn't exist.
+/// \param ht
+/// \param key
+/// \return
 void *ht_get(hashtable_t *ht, char *key) {
   unsigned int idx = hash(key) % ht->size;
   bucket_t *b = ht->buckets[idx];
@@ -43,6 +54,9 @@ void *ht_get(hashtable_t *ht, char *key) {
   return NULL;
 }
 
+/// Calls f with all key/value mappings in the hashtable; iteration can be terminated early if f returns 0.
+/// \param ht
+/// \param f
 void ht_iter(hashtable_t *ht, int (*f)(char *, void *)) {
   bucket_t *b;
   unsigned long i;
@@ -57,13 +71,21 @@ void ht_iter(hashtable_t *ht, int (*f)(char *, void *)) {
   }
 }
 
+/// Frees all keys, values, buckets, and the underlying bucket array of the hashtable.
+/// \param ht
 void free_hashtable(hashtable_t *ht) {
   free(ht); // FIXME: must free all substructures!
 }
 
 /* TODO */
+/// Removes the mapping for key
+/// \param ht
+/// \param key
 void  ht_del(hashtable_t *ht, char *key) {
 }
 
+/// Resizes the hashtable to contain newsize buckets, rehashing all keys and moving them into new buckets as needed.
+/// \param ht
+/// \param newsize
 void  ht_rehash(hashtable_t *ht, unsigned long newsize) {
 }

61bf0bbb6100fb914368ad17f7df71359e8b84ba
HieuNguyen202 <edwardnguyen202@gmail.com>
2019-03-17 12:12:37 -0500

    add print_help() to print the help message to stdout

--- mps/04/csim.c
+++ mps/04/csim.c
@@ -1,7 +1,28 @@
 #include "cachelab.h"
+#include <stdio.h>
+char* help_message = "./csim-ref: Missing required command line argument\n"
+                     "Usage: ./csim-ref [-hv] -s <num> -E <num> -b <num> -t <file>\n"
+                     "Options:\n"
+                     "  -h         Print this help message.\n"
+                     "  -v         Optional verbose flag.\n"
+                     "  -s <num>   Number of set index bits.\n"
+                     "  -E <num>   Number of lines per set.\n"
+                     "  -b <num>   Number of block offset bits.\n"
+                     "  -t <file>  Trace file.\n"
+                     "\n"
+                     "Examples:\n"
+                     "  linux>  ./csim-ref -s 4 -E 1 -b 4 -t traces/yi.trace\n"
+                     "  linux>  ./csim-ref -v -s 8 -E 2 -b 4 -t traces/yi.trace";
+
+void print_help();  //Print help message
 
 int main()
 {
+    print_help();
     printSummary(0, 0, 0);
     return 0;
+}
+
+void print_help(){
+    printf("%s", help_message);
 }

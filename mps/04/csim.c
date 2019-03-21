add60b9a8318ad3aba179cc86d926ca2eb0562fa
HieuNguyen202 <edwardnguyen202@gmail.com>
2019-03-20 20:43:18 -0500

    Implemented option and argument parser.

--- mps/04/csim.c
+++ mps/04/csim.c
@@ -1,7 +1,10 @@
 #include "cachelab.h"
 #include <stdio.h>
-char* help_message = "./csim-ref: Missing required command line argument\n"
-                     "Usage: ./csim-ref [-hv] -s <num> -E <num> -b <num> -t <file>\n"
+#include <getopt.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+char* help_message = "Usage: ./csim [-hv] -s <num> -E <num> -b <num> -t <file>\n"
                      "Options:\n"
                      "  -h         Print this help message.\n"
                      "  -v         Optional verbose flag.\n"
@@ -16,10 +19,47 @@ char* help_message = "./csim-ref: Missing required command line argument\n"
 
 void print_help();  //Print help message
 
-int main()
+int main(int argc, char** argv)
 {
+    char *optString = "hvs:E:b:t:";
+    extern char* optarg;
+    extern int optind, opterr, optopt;
+    int f_verbose, num_lines_per_set, num_set_index_bits, num_block_bits;
+    char* trace_file;
+    int c;
+    while ((c=getopt(argc, argv, optString))!=-1){
+        switch(c){
+            case 'h':
+                print_help();
+                exit(0);
+            case 'v':
+                f_verbose = 1;
+                break;
+            case 's':
+                //TODO: make sure the input contains valid numeric
+                num_lines_per_set = atoi(optarg);
+                break;
+            case 'E':
+                //TODO: make sure the input contains valid numeric
+                num_lines_per_set = atoi(optarg);
+                break;
+            case 'b':
+                //TODO: make sure the input contains valid numeric
+                num_block_bits = atoi(optarg);
+                break;
+            case 't':
+                trace_file = optarg;
+                break;
+            default:    //'?'
+                //TODO: devide what to do by default
+                printf("./csim: Missing required command line argument\n");
+                print_help();
+                exit(0);
+        }
+    }
+
     //TODO: add command parser
-    print_help();
+    //TODO: Run csim-ref with different flags to observe its behaviors
     printSummary(0, 0, 0);
     return 0;
 }

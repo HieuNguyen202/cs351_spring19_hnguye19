7341dd59838cac624ea74ce5ae36f660c74c369a
HieuNguyen202 <edwardnguyen202@gmail.com>
2019-04-12 14:47:55 -0500

    Debugs

--- mps/04/trans.c
+++ mps/04/trans.c
@@ -48,8 +48,8 @@ void trans(int M, int N, int A[N][M], int B[M][N])
 
 void mytrans(int M, int N, int A[N][M], int B[M][N], int dy, int dx){
     int i, j, ii, jj, h;
-    for (j = 0; j<M; j+=dy) {
-        for (i = 0; i<N; i+=dx) {
+    for (i = 0; i<N; i+=dx) {
+        for (j = 0; j<M; j+=dy) {
 //            if (i == j) {
                 for (jj = 0; jj < dy; ++jj) {
                     for (ii = 0; ii < dx; ++ii) {
